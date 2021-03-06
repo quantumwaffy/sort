2330feb107d2c2f8895bf14bb9c40d313f658fb7
Sasha <sashchystyh@yandex.ru>
2021-03-06 16:19:51 +0300

    Installed Postgre and added algorithms of sorting data

--- sort/settings.py
+++ sort/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'sort.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'task',
+        'USER': 'admin',
+        'PASSWORD': '1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Istanbul'
 
 USE_I18N = True
 
---
+++ task/sort.py
@@ -0,0 +1,86 @@
+from abc import ABC, abstractmethod
+
+class SortData(ABC):
+    @abstractmethod
+    def sort(self, list):
+        pass
+
+    def getdata(self, file):
+        with open(file,"r") as f:
+            for line in f.readlines():
+                self.list = [int(item) for item in line.split(',')]
+        return self.list
+
+    def checkint(self, list):
+        if all(isinstance(elem, int) for elem in list):
+            return True
+        else:
+            print("List have not int elements")
+            return False
+
+
+class BubleSort(SortData):
+    def sort(self, list):
+        if super().checkint(list):
+            for i in range(len(list) - 1):
+                for j in range(len(list) - i - 1):
+                    if list[j] > list[j + 1]:
+                        list[j], list[j + 1] = list[j + 1], list[j]
+            return list
+        else:
+            return "Error of list in BubleSort"
+
+
+class SelectionSort(SortData):
+    def sort(self, list):
+        if super().checkint(list):
+                for i in range(len(list)):
+                    lowest_index = i
+                    for j in range(i + 1, len(list)):
+                        if list[j] < list[lowest_index]:
+                            lowest_index = j
+                    list[i], list[lowest_index] = list[lowest_index], list[i]
+        return list
+
+
+class InsertionSort(SortData):
+    def sort(self, list):
+        if super().checkint(list):
+            for i in range(1, len(list)):
+                item_ins = list[i]
+                j = i - 1
+                while j >= 0 and list[j] > item_ins:
+                    list[j + 1] = list[j]
+                    j -= 1
+                list[j + 1] = item_ins
+        return list
+
+class MergeSort(SortData):
+    def merge_sort(self,left_list, right_list):
+        list = []
+        left_list_index = right_list_index = 0
+        left_list_length, right_list_length = len(left_list), len(right_list)
+        for i in range(left_list_length + right_list_length):
+            if left_list_index < left_list_length and right_list_index < right_list_length:
+                if left_list[left_list_index] <= right_list[right_list_index]:
+                    list.append(left_list[left_list_index])
+                    left_list_index += 1
+                else:
+                    list.append(right_list[right_list_index])
+                    right_list_index += 1
+            elif left_list_index == left_list_length:
+                list.append(right_list[right_list_index])
+                right_list_index += 1
+            elif right_list_index == right_list_length:
+                list.append(left_list[left_list_index])
+                left_list_index += 1
+        return list
+
+    def sort(self, list):
+        if super().checkint(list):
+            if len(list) <= 1:
+                return list
+            mid = len(list) // 2
+            left_list = self.sort(list[:mid])
+            right_list = self.sort(list[mid:])
+        return self.merge_sort(left_list, right_list)
